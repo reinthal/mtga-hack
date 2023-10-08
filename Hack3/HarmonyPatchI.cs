ca3c27c53e4bac6afdd370794f62c018836a8ba8
frogman <frog.man@pond.com>
2023-10-08 02:08:22 -0700

    wip create interface to DRY the code

---
+++ Hack3/HarmonyPatchI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack3
+{
+    internal interface HarmonyPatchI
+    {
+        void DoPatching();
+        void UndoPatching();
+    }
+}
